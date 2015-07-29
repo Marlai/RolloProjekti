f35c682bc504c9067407ecd9a25862de8acfcdf7
marlai <laitinen.marko@hotmail.com>
2015-07-29 09:01:02 +0300

    newer files

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 
-app.get('/', function (req, res){
-   res.send('Hello World') 
+//called for every request
+app.use(function(req,res,next){
+    console.log(req.method);
+    next();
+});
+
+//serve static files from public folder
+app.use('/', express.static('public'));
+
+//parse body from post
+app.use(bodyParser());
+
+app.post('/contact',function(req,res){
+   console.log(req.body.user_name);
+   res.send("Your data is saved");
 });
 
 app.listen(3000);
